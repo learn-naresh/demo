6022024e27cd6f8770cd45b4703680c925c37116
Naresh Verma <learnnaresh7566@gmail.com>
2024-05-06 18:33:25 +0530

    Initial commit with tailwind css added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // Import React
+import './App.css';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <Body/>
+  );
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Body = () => {
+  return (
+    <Navbar/>
+  )
+}
+
+export default Body
